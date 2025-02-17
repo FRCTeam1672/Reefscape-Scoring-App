d7a1c403875b0d871489b7c40a077206da060237
Ishaan <68785503+EmeraldWither@users.noreply.github.com>
2025-02-16 21:18:00 -0500

    visual tweaks

--- lib/comms/usb_comms.dart
+++ lib/comms/usb_comms.dart
@@ -1,21 +1,20 @@
 import 'package:reefscape_scoring_robotbirds/comms/comms.dart';
 
 class USBComms implements Comms{
+
   @override
   bool isConnected() {
-    // TODO: implement isConnected
-    throw UnimplementedError();
+    return false;
   }
 
   @override
   String onDataReceive(Function() callback) {
-    // TODO: implement onDataReceive
-    throw UnimplementedError();
+    return "";
   }
 
   @override
   void sendData(String reef, int level, int coralStation, int algae) {
-    // TODO: implement sendData
+
   }
 
   @override
@@ -25,12 +24,12 @@ class USBComms implements Comms{
 
   @override
   void init() {
-    // TODO: implement init
   }
+
 
   @override
   void sendAllData() {
-    // TODO: implement sendAllData
+
   }
 
 }
--- lib/scoring_page/status_bar.dart
+++ lib/scoring_page/status_bar.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_gradient_animation_text/flutter_gradient_animation_text.dart';
 import 'package:reefscape_scoring_robotbirds/comms/comms_manager.dart';
 
 class StatusBar extends StatefulWidget {
@@ -9,16 +10,24 @@ class StatusBar extends StatefulWidget {
   final Color color2;
   final int duration;
   final CommsManager comms;
-  const StatusBar({super.key, required this.color1, required this.color2, required this.duration, required this.comms});
+
+  const StatusBar(
+      {super.key,
+      required this.color1,
+      required this.color2,
+      required this.duration,
+      required this.comms});
 
   @override
   State<StatusBar> createState() => _StatusBarState();
 }
 
-class _StatusBarState extends State<StatusBar> {
+class _StatusBarState extends State<StatusBar>
+    with SingleTickerProviderStateMixin {
   Color color = Colors.grey;
 
   @override
+  @mustCallSuper
   void initState() {
     scheduleBackground();
     super.initState();
@@ -29,8 +38,8 @@ class _StatusBarState extends State<StatusBar> {
       Duration(milliseconds: widget.duration),
       (timer) {
         setState(() {
-          if(widget.comms.comms.isConnected()) {
-            color = Color.fromRGBO(0, 255, 0, 1);
+          if (widget.comms.comms.isConnected()) {
+            // color = Color.fromRGBO(0, 255, 0, 1);
           } else {
             color = color == widget.color1 ? widget.color2 : widget.color1;
           }
@@ -51,6 +60,7 @@ class _StatusBarState extends State<StatusBar> {
           padding: const EdgeInsets.all(8),
           child: Row(
             children: [
+              Image.asset("assets/team_logo.png", scale: 7),
               Text(
                 "Robo T-Birds 1672",
                 style: TextStyle(fontFamily: "BebasNeue", fontSize: 36),
@@ -61,6 +71,13 @@ class _StatusBarState extends State<StatusBar> {
               Image.asset(
                 "assets/reefscape_logo.png",
                 scale: 30,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Text(
+                "Scoring System",
+                style: TextStyle(fontFamily: "BebasNeue", fontSize: 36),
               ),
             ],
           ),
@@ -73,11 +90,7 @@ class _StatusBarState extends State<StatusBar> {
           padding: const EdgeInsets.all(8),
           child: Row(
             children: [
-              Text(
-                widget.comms.comms.isConnected() ? "CONNECTED" : "NO COMMS",
-                style: TextStyle(
-                    fontFamily: "BebasNeue", fontSize: 30, color: color),
-              ),
+              getConnectedText(),
               SizedBox(
                 width: 50,
               ),
@@ -94,12 +107,42 @@ class _StatusBarState extends State<StatusBar> {
   }
 
   Widget getReconnectButton() {
-    if(widget.comms.comms.isConnected()) return const SizedBox.shrink();
-    return FilledButton(onPressed: () {
-      widget.comms.setupComms();
-      setState(() {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Attempted to reconnect by re-initializing comms. ${Platform.isWindows ? "\nReconnecting to NT4 on 10.16.72.2." : "Reopening USB Connection"}")));
-      });
-    }, child: Text("Re-init Comms"));
+    if (widget.comms.comms.isConnected()) return const SizedBox.shrink();
+    return FilledButton(
+        onPressed: () {
+          widget.comms.setupComms();
+          setState(() {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(
+                    "Attempted to reconnect by re-initializing comms. ${Platform.isWindows ? "\nReconnecting to NT4 on 10.16.72.2." : "Reopening USB Connection"}")));
+          });
+        },
+        child: Text("Re-init Comms"));
+  }
+
+  Widget getConnectedText() {
+    if (!widget.comms.comms.isConnected()) {
+      return Text(
+        "NO COMMS",
+        style: TextStyle(fontFamily: "BebasNeue", fontSize: 30, color: color),
+      );
+    } else {
+      return GradientAnimationText(
+        text: Text(
+          'CONNECTED',
+          style: TextStyle(
+              fontFamily: "BebasNeue", fontSize: 30
+          ),
+        ),
+        colors: [
+          Color(0xff8f00ff),  // violet
+          Colors.green,
+          Colors.yellow,
+          Colors.orange,
+          Colors.red,
+        ],
+        duration: Duration(milliseconds: 1000),
+      );
+    }
   }
 }
